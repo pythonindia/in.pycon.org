ada7e1d92d3102445b2b2243abbffe64fd4684fe
Anand Chitipothu <anandology@gmail.com>
2010-06-09 10:28:43 +0530

    removed old stuff.

--- plugins/pycon/code.py
+++
@@ -1,7 +0,0 @@
-from infogami.utils import delegate
-from infogami.utils.template import render
-
-class earlybird_registration(delegate.page):
-    def POST(self):
-        return render.earlybird()
-
