e09b97365c1c7ae66d15b1f66d7e3decfcb05b76
Anand Chitipothu <anandology@gmail.com>
2010-06-10 22:58:39 +0530

    Added validation to submit talk form.

--- plugins/conference/code.py
+++ plugins/conference/code.py
@@ -2,20 +2,38 @@ from infogami.utils import delegate
 from infogami.utils.view import render, add_flash_message
 from infogami import config
 import web
+from web.form import Form, Textbox, Textarea, notnull, regexp
 
 def render_template(name, *a, **kw):
     if "." in name:
         name = name.rsplit(".", 1)[0]
     return render[name](*a, **kw)
+    
+re_email = r'^[\w\.-]+@[\w\.-]+\.[a-zA-Z]{2,4}$'
+    
+form_talk = Form(
+    Textbox("title", notnull),
+    Textbox("duration", notnull),
+    Textbox("authors", notnull),
+    Textbox("contact", notnull, regexp(re_email, "Please enter a valid email address.")),
+    Textbox("level", notnull),
+    Textbox("tags"),
+    Textarea("summary", notnull),
+)
 
 class submit_talk(delegate.page):
     path = "/talks/submit"
     
     def GET(self):
-        return render_template("talks/submit")
+        f = form_talk()
+        return render_template("talks/submit", f)
         
     def POST(self):
         i = web.input()
+        f = form_talk()
+        
+        if not f.validates(i):
+            return render_template("talks/submit", f)
         
         if config.get('from_address') and config.get('talk_submission_contact'):
             email = render_template("talks/email", i)
