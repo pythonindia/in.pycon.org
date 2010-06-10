69dea835817c2aadce419b6a1a754409c85bc16c
Anand Chitipothu <anandology@gmail.com>
2010-06-10 16:35:41 +0530

    Send email on talk submission.

--- plugins/conference/code.py
+++ plugins/conference/code.py
@@ -1,5 +1,6 @@
 from infogami.utils import delegate
 from infogami.utils.view import render, add_flash_message
+from infogami import config
 import web
 
 def render_template(name, *a, **kw):
@@ -11,10 +12,17 @@ class submit_talk(delegate.page):
     path = "/talks/submit"
     
     def GET(self):
-        add_flash_message("error", "Talk submission is not yet ready. Please check again after a couple days.")  
         return render_template("talks/submit")
         
     def POST(self):
-        add_flash_message("error", "Talk submission is not yet ready. Please check again after a couple days.")
         i = web.input()
-        return render_template("talks/submit", i)
+        
+        if config.get('from_address') and config.get('talk_submission_contact'):
+            email = render_template("talks/email", i)
+            web.sendmail(
+                from_address=config.from_address, 
+                to_address=config.talk_submission_contact,
+                subject=web.safestr(email.subject.strip()),
+                message=web.safestr(email)
+            )
+        return render_template("talks/submitted")
