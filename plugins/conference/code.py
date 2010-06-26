4b23fa88710e965bb399e4c0267881133e0b71c2
Anand Chitipothu <anandology@gmail.com>
2010-06-27 00:53:23 +0530

    Added code to display talks in the website.

--- plugins/conference/code.py
+++ plugins/conference/code.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from infogami.utils import delegate
-from infogami.utils.view import render, add_flash_message
+from infogami.utils.view import render, add_flash_message, public
 from infogami import config
 import web
 from web.form import Form, Textbox, Textarea, notnull, regexp, Validator
@@ -10,6 +10,7 @@ import simplejson
 
 from tools import captcha
 
+@public
 def render_template(name, *a, **kw):
     if "." in name:
         name = name.rsplit(".", 1)[0]
@@ -33,6 +34,21 @@ form_talk = Form(
     Textarea("notes"),
 )
 
+import threading
+talk_lock = threading.Lock()
+
+def new_talk(talk):
+    talk['type'] = 'talk'
+
+    talk_lock.acquire()
+    try:
+        index = 1 + len(web.ctx.site.store.keys(type='talk', limit=1000))
+        key = "talks/%d" % index
+        web.ctx.site.store[key] = talk
+    finally: 
+        talk_lock.release()
+    return key
+
 class submit_talk(delegate.page):
     path = "/talks/submit"
     
@@ -54,6 +70,9 @@ class submit_talk(delegate.page):
             c = captcha.displayhtml(public_key="6LeHGbsSAAAAAPwlmoQCVUYlP_vip_iXv5vAAcEN")
             return render_template("talks/submit", form=f, captcha=c, captcha_error = "Are you human?")
                 
+            
+        key = new_talk(i)
+        
         if config.get('from_address') and config.get('talk_submission_contact'):
             email = render_template("talks/email", i)
             web.sendmail(
@@ -65,8 +84,41 @@ class submit_talk(delegate.page):
 
         dir = config.get("talks_dir", "/tmp/talks")
         write("%s/%s.txt" % (dir, time.time()), simplejson.dumps(i))
+        
+        add_flash_message("info", "Thanks for submitting your talk. The selection committee will review your talk and get in touch with you shortly.")
+        raise web.seeother("/" + key)
+        
+class display_talk(delegate.page):
+    path = "/talks/(\d+)(.*)"
+    
+    def GET(self, id, title):
+        title = title.lstrip("-").replace("-", " ")
+        try:
+            talk = web.ctx.site.store["talks/" + id]
+        except KeyError:
+            raise web.notfound()
+            
+        xtitle = talk.get('title', 'untitled')
+        if xtitle != title:
+            path = "/talks/%s-%s" % (id, xtitle.replace(" ", "-"))
+            raise web.redirect(path)
+            
+        talk = web.storage(talk)
+        return render_template("talks/view", talk)
+        
+class talks(delegate.page):
+    def GET(self):
+        i = web.input()
+        items = web.ctx.site.store.items(type='talk', limit=1000)
+        items = list(items)
+        
+        if 'topic' in i:
+            items = [(k, talk) for k, talk in items if talk.get('topic') == i.topic]
 
-        return render_template("talks/submitted")
+        if 'level' in i:
+            items = [(k, talk) for k, talk in items if talk.get('level') == i.level]
+        
+        return render_template("talks/index", items)
 
 def write(path, text):
     dirname = os.path.dirname(path)
