769b06d34a4cbf8bdc5a3a235633e568ab2de232
Anand Chitipothu <anandology@gmail.com>
2010-06-28 13:06:05 +0530

    tweet when talk is submitted.

--- plugins/conference/code.py
+++ plugins/conference/code.py
@@ -8,6 +8,11 @@ import os
 import time
 import simplejson
 
+try:
+    import twitter
+except ImportError:
+    twitter = None
+
 @public
 def render_template(name, *a, **kw):
     if "." in name:
@@ -46,6 +51,18 @@ def new_talk(talk):
     finally: 
         talk_lock.release()
     return key
+    
+def tweet_talk(title, author, url):
+    t = config.get("twitter", {})
+    if twitter and t.get('username') and t.get('password'):
+        api = twitter.Api(t['username'], t['password'])
+        
+        # limiting title and author length to avoid crossing 140 char limit by twitter
+        title = title[:50]
+        author = author[:25]
+        
+        template = t.get("template", "New talk submitted: %(title)s By %(author)s %(url)s")
+        api.PostUpdate(template % locals())
 
 class submit_talk(delegate.page):
     path = "/talks/submit"
@@ -74,6 +91,12 @@ class submit_talk(delegate.page):
 
         dir = config.get("talks_dir", "/tmp/talks")
         write("%s/%s.txt" % (dir, time.time()), simplejson.dumps(i))
+        
+        try:
+            tweet_talk(i.title, i.authors, web.ctx.home + "/" + key)
+        except:
+            import traceback
+            traceback.print_exc()
         
         add_flash_message("info", "Thanks for submitting your talk. The selection committee will review your talk and get in touch with you shortly.")
         raise web.seeother("/" + key)
